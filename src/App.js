eeeb32783dbcf621be7cb020080d3bf2e2c43189
Chheng <oeungchungchheng2205@gmail.com>
2022-12-06 11:26:22 +0700

    set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-400">
+        <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
